e52550b0f959a545d64cf8b183775f6a13f5ac74
MohamedFares09 <mofares010@gmail.com>
2026-02-10 22:08:58 +0200

    H.W lesson 12

--- course3/main.cpp
+++ course3/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main(){
-    std:: cout<<"Moahemd Fares";
+    std:: cout << "********************************\n";
+    std :: cout << "Name : Mohamed Fares \n";
+    std ::  cout << "Age : 24"<<std:: endl ;
+    std :: cout << "City  : Kafr Elzayat \n" ;
+    std :: cout << "Country : Egypt \n" ;
     return 0 ;
 }
