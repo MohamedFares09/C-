51a2e23a91e119cf39a88c7daaa8f822d41d9506
MohamedFares09 <mofares010@gmail.com>
2026-02-10 22:17:28 +0200

    H.W 4 lesson 12

--- course3/main.cpp
+++ course3/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 int main(){
-    std :: cout << "I Love Programming\n\n";
-    std :: cout << "I Promise to be the best developer ever!\n\n";
-    std :: cout << "I know it will take some tiime to practice . but I will achieve my goal.\n\n";
-    std :: cout << "Best Regards\nMohamed Abu-Hahdoud";
+    std :: cout << "*    *\n";
+    std :: cout << "*    *\n";
+    std :: cout << "******\n";
+    std :: cout << "*    *\n"; 
+    std :: cout << "*    *\n"; 
     return 0 ;
 }
