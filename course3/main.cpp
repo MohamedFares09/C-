478f94b1d87e1cefaf9cb320d40c838a2947f78f
Mohamed Fares <mofares010@gmail.com>
2026-02-15 01:48:37 +0200

    Home work 2 lesson 16

--- course3/main.cpp
+++ course3/main.cpp
@@ -3,26 +3,34 @@ using namespace std ;
 
 int main(){
 
-    
-string Name = "Mohamed Abo-Hadhoud ";
-int Age = 44 ;
-string City = "Ammon";
-string Country = "Jardan";
-int Monthly_Salary = 5000; 
-int Yearly_Salary = 12*Monthly_Salary;
-char Gender = 'M';
-bool Married = true ;
+int num1 = 20 ; 
+int num2 = 30 ; 
+int num3 = 10 ;
+cout << "Total = "<< num1+num2+num3;
 
-cout << "************************";
-cout << "Name : "<< Name<<endl;
-cout << "Age : "<< Age << "Years "<<endl;
-cout << "City  : "<< City<<endl;
-cout << "Country : "<< Country<<endl;
-cout << "Monthly_Salary : " << Monthly_Salary<< endl ;
-cout << "Yearly_Salary : "<< Yearly_Salary << endl;
-cout << "Gender : " << Gender << endl ;
-cout << "Married : "<< Married ;
-cout<< "********************************";
+
+
+
+
+// string Name = "Mohamed Abo-Hadhoud ";
+// int Age = 44 ;
+// string City = "Ammon";
+// string Country = "Jardan";
+// int Monthly_Salary = 5000; 
+// int Yearly_Salary = 12*Monthly_Salary;
+// char Gender = 'M';
+// bool Married = true ;
+
+// cout << "************************";
+// cout << "Name : "<< Name<<endl;
+// cout << "Age : "<< Age << "Years "<<endl;
+// cout << "City  : "<< City<<endl;
+// cout << "Country : "<< Country<<endl;
+// cout << "Monthly_Salary : " << Monthly_Salary<< endl ;
+// cout << "Yearly_Salary : "<< Yearly_Salary << endl;
+// cout << "Gender : " << Gender << endl ;
+// cout << "Married : "<< Married ;
+// cout<< "********************************";
 
     return 0 ;
 }
