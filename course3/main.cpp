b90ad70c3e27b595ba9cdfd39433081ba2e44ebe
Mohamed Fares <mofares010@gmail.com>
2026-02-15 01:46:50 +0200

    Home work 1 lesson 17

--- course3/main.cpp
+++ course3/main.cpp
@@ -2,10 +2,27 @@
 using namespace std ;
 
 int main(){
-     cout << "Ali \t Ahmed \t lena "<<endl;
-     cout << "Fadi \t Zain \t Mona "<<endl;
-      
+
     
-    
+string Name = "Mohamed Abo-Hadhoud ";
+int Age = 44 ;
+string City = "Ammon";
+string Country = "Jardan";
+int Monthly_Salary = 5000; 
+int Yearly_Salary = 12*Monthly_Salary;
+char Gender = 'M';
+bool Married = true ;
+
+cout << "************************";
+cout << "Name : "<< Name<<endl;
+cout << "Age : "<< Age << "Years "<<endl;
+cout << "City  : "<< City<<endl;
+cout << "Country : "<< Country<<endl;
+cout << "Monthly_Salary : " << Monthly_Salary<< endl ;
+cout << "Yearly_Salary : "<< Yearly_Salary << endl;
+cout << "Gender : " << Gender << endl ;
+cout << "Married : "<< Married ;
+cout<< "********************************";
+
     return 0 ;
 }
